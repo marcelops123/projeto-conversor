c0bde05a14b3d9db121029bfd89a583741f37d3e
Marcelin <marceloaugusto10123@gmail.com>
2022-02-24 16:38:10 -0300

    update

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+import Conversor from "./components/Conversor"
+
+function App() {
+  return (
+    <body className='sla'>
+      <div className="App">
+      <h1>Conversor de Moedas</h1>
+      <div className="linha">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+    
+    <div className="linha">
+    <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+    <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+    </div>
+    <div className="linha">
+    <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+    <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+    </div>
+    </div>
+    </body>
+  );
+}
+
+export default App;
